b37f911ae269bee2c845c27efaef8f3b613c7745
rinshadpdev <rinshadp014@gmail.com>
2023-07-05 14:47:38 +0530

    get user choice
    copell user to give input untill it is correct choice & change first letter of choice into the UPpercase

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
+let choices = ['Rock', 'Paper', 'Scissors']
  //i have to computer choice first which will play 3 round;this is function
 function getComputerChoice() {
-    let choices = ['Rock', 'Paper', 'Scissors']
     let choice = (Math.round(Math.random()*3))
-    console.log(choices[choice])
-
+   
+   return choices[choice]
 }
 
-getComputerChoice()
-//i have to prompt for the user choice second within function and return  this is another function
-//i have to compare computer choice and user choice to determine the each round result
-// thne i have to show who won the game 
+// prompt for the user choice and return it 
+function getuserChoice() {
+   let selection;
+do {
+    selection = prompt("Please choose your weapon: Rock, Paper,siccors");
+    selection = selection.charAt(0).toUpperCase() + selection.slice(1);
+    if (!choices.includes(selection)) {
+        alert("Invalid choice. Please choose the correct one");
+      }
+  } while (!choices.includes(selection));
+  return selection;
+}
+let userSelection = getuserChoice()
+let computerSelection = getComputerChoice()
+console.log(userSelection)
+console.log(computerSelection)
+
