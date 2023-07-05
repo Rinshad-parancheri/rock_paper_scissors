a6c7d30352e079da1aa7f15ad8ab9d90ee966227
rinshadpdev <rinshadp014@gmail.com>
2023-07-05 13:45:25 +0530

    add project starter

---
+++ main.js
@@ -0,0 +1,4 @@
+//i have to computer choice first which will play 3 round;
+//i have to prompt for the user choice second within function and return 
+//i have to compare computer choice and user choice to determine the each round result
+// thne i have to show who won the game 
